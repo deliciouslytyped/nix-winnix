df00757cbc4893271e0d322bb79d99e633465a9f
deliciouslytyped <deliciouslytyped>
2019-04-29 03:27:10 +0200

    change default example, commit uncommitted deletions

--- shell.nix
+++ shell.nix
@@ -3,5 +3,6 @@ with import <nixpkgs> {};
    pp = callPackage ./lib/packages.nix {};
    setPath = self: super: { path = builtins.toString ./mount; }; #TODO hack
   in
-    (pp.extend setPath).withPackages (p: with p; [ p.".kkrieger" ])
+    (pp.extend setPath).withPackages (p: with p; [ cs2d ])
+#    (pp.extend setPath).withPackages (p: with p; [ p.".kkrieger" ])
 #    (pp.extend setPath).withPackages (p: with p; [ p."explorer++" cs2d ])
